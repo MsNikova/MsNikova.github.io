06eece88d0be3015497059dad558194535a9f1e7
Diana Nikova <nickova92@gmail.com>
2022-01-18 22:20:24 +0200

    Revert "scss loader"
    This reverts commit 334bbcf6830be8270d7e366e802f92c61eb27d86.

--- vivastyle/webpack.config.js
+++ vivastyle/webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
     entry: {
         main: './index.js'
     },
@@ -14,48 +12,14 @@ module.exports = {
         new HTMLWebpackPlugin({
             filename: './index.html',
             template: './index.html'
-        }),
-        new MiniCssExtractPlugin(),
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: ["style-loader", "css-loader"],
             },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                  'file-loader',
-                  {
-                    loader: 'image-webpack-loader',
-                    options: {
-                      mozjpeg: {
-                        progressive: true,
-                      },
-                      // optipng.enabled: false will disable optipng
-                      optipng: {
-                        enabled: false,
-                      },
-                      pngquant: {
-                        quality: [0.65, 0.90],
-                        speed: 4
-                      },
-                      gifsicle: {
-                        interlaced: false,
-                      },
-                      // the webp option will enable WEBP
-                      webp: {
-                        quality: 75
-                      }
-                    }
-                  },
-                ],
-              },
         ]
     }
 }
